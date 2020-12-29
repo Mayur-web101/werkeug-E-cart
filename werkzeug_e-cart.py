16e9d1a367971fb79e473224094529e627c006fc
Mayur-web101 <mayur101gvp@gmail.com>
2020-12-29 15:46:26 +0530

    [IMP] werkzeug_e-cart

--- werkzeug_e-cart.py
+++ werkzeug_e-cart.py
@@ -84,6 +84,7 @@ def application(request):
                     </html>
                         ''')
     return Response(''.join(result),mimetype='text/html')
+
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('localhost',4000,application)
