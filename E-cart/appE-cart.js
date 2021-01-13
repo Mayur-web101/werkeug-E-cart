68bff68f19bc2884e8b2b95d71452849fb37a2e6
Mayur-web101 <mayur101gvp@gmail.com>
2021-01-13 16:35:53 +0530

    [WIP] improve

--- E-cart/appE-cart.js
+++ E-cart/appE-cart.js
@@ -3,6 +3,23 @@ const { xml } = owl.tags;
 const { whenReady } = owl.utils;
 const { useRef, useDispatch, useState, useStore } = owl.hooks;
 
+const actions = {
+  addToCart({ state }, poduct_id) {
+        if (poduct_id && (!state.cartData.find(t => parseInt(t.poduct_id) == parseInt(poduct_id))))
+         {
+            const newTask ={
+                poduct_id: poduct_id,
+            };
+            state.cartData.push(newTask);
+         }
+  },
+  
+  deleteTask({ state }, poduct_id) 
+  {
+     const index = state.cartData.findIndex((t) => t.poduct_id == poduct_id);
+        state.cartData.splice(index, 1);
+  },
+};   
 const APP_TEMPLATE = xml /* xml */ `
 <div>
     <div id="task-list">
@@ -24,40 +41,43 @@ const APP_TEMPLATE = xml /* xml */ `
         <table>
             <h2>CART</h2>
             <t t-set="total" t-value="0"/>
-            <t t-foreach="cartData" t-as="i" t-key="i.id"> 
-               <t t-set="total" t-value="total + tasks[i.poduct_id-1].Price"/>
-                <tr>
-               <td><span><t t-esc="tasks[i.poduct_id-1].Product1"/></span></td>
-               <td><span><t t-esc="tasks[i.poduct_id-1].Price"/></span></td>
-               <td><span class="task-list" t-att-id="i.poduct_id" t-on-click="deleteTask">🗑</span></td>
-               </tr>
+            <t t-if="cartData">
+                <t t-foreach="cartData" t-as="i">
+                   <t t-set="total" t-value="total + tasks[i.poduct_id-1].Price"/>
+                   <tr>
+                   <td><span><t t-esc="tasks[i.poduct_id-1].Product1"/></span></td>
+                   <td><span><t t-esc="tasks[i.poduct_id-1].Price"/></span></td>
+                   <td><span class="task-list" t-att-id="i.poduct_id" t-on-click="deleteTask">🗑</span></td>
+                   </tr>
+                </t>
             </t>
             <span>Total: <t t-esc="total"/></span>
         </table>
     </div>
 </div>`;
 
+const initialState = {
+    cartData: []
+};
 // Owl Components
+
 class App extends Component 
 {
     static template = APP_TEMPLATE;  
+    cartData = useStore((state) => state.cartData) 
+    dispatch = useDispatch();
+
     addToCart(ev)
     {
-        const poduct_id = ev.target.id;
-        if (poduct_id && (!this.cartData.find(t => parseInt(t.poduct_id) == parseInt(poduct_id))))
-         {
-            const newTask = 
-            {
-                poduct_id: poduct_id,
-            };
-            this.cartData.push(newTask);
-         }
+        if(ev.target.id)
+        {
+            this.dispatch("addToCart",ev.target.id)
+        }
     }
 
     deleteTask(ev)
     {
-        const index = this.cartData.findIndex(t => t.poduct_id === ev.target.id);
-        this.cartData.splice(index, 1);
+        this.dispatch("deleteTask",ev.target.id)
     }
 
     search()
@@ -65,7 +85,6 @@ class App extends Component
         var input, filter, mainDiv, div, childDiv, i, txtValue;
         input = document.getElementById("search").value;
         filter = input.toUpperCase();
-        console.log(filter);
         mainDiv = document.getElementById("task-list");
         div = mainDiv.getElementsByClassName("task");
         for (i = 0; i < div.length; i++) 
@@ -85,7 +104,7 @@ class App extends Component
             }
         }
     }
-    cartData = useState([]);
+
     tasks = [
             {
                 "id": 1,
@@ -103,20 +122,24 @@ class App extends Component
                 "Price":300
             }
         ];
-}   
+}  
+
+
 function makeStore() 
 {
-    const localState = window.localStorage.getItem("todoapp");
-    const state = localState ? JSON.parse(localState) : initialState;
-    const store = new Store({ state});
-    store.on("update", null, () => {
-    localStorage.setItem("todoapp", JSON.stringify(store.state));
-});
+   const localState = window.localStorage.getItem("shopping");
+   const state = localState ? JSON.parse(localState) : initialState;
+   const store = new Store({ state, actions });
+   store.on("update", null, () => {
+       localStorage.setItem("shopping", JSON.stringify(store.state));
+   });
+   debugger;
 return store;
 }
 
 function setup() 
 {
+     owl.config.mode = "dev";
     App.env.store = makeStore();
     const app=new App();
     app.mount(document.body);
