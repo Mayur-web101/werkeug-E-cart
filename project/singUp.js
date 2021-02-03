582eab3bf339c5cc7be720922f065324d8028b33
Mayur-web101 <mayur101gvp@gmail.com>
2021-02-03 18:49:09 +0530

    [ADD] Added 1 new  project Files

---
+++ project/singUp.js
@@ -0,0 +1,30 @@
+const { Component,Store,mount } = owl;
+const { xml } = owl.tags;
+
+
+export class SignUp extends Component
+{
+	static Sign_Template = xml`
+	<div>
+		<div>
+				<h2>Sign Up</h2>
+				<div>
+					<label for="email" class="email">USERNAME</label>
+					<input id="email" placeholder="Email OR Mobile" type="text"></input>
+				</div>
+				<div>
+					<label for="password" class="password">PASSWORD</label>
+					<input id="password" placeholder="password" type="password"></input>
+				</div>
+				<div>
+					<label for="conpassword" class="conpassword">Confirm PASSWORD</label>
+					<input id="conpassword" placeholder="confrim password" type="password"></input>
+				</div>
+				<div>
+					<label for="mobile" class="mobile">MOBILE</label>
+					<input id="mobile" class="mobile" placeholder="Mobile" type="number"></input>
+				</div>
+				<button>Sign Up</button>
+		</div>
+	</div>`;
+}
