75a2ee2789a9127f78493491d1471b9e9ae1375e
Mayur-web101 <mayur101gvp@gmail.com>
2021-03-01 18:54:56 +0530

    contain change in two file

--- Modern Mondy Bazar/project/Cropedetail.js
+++ Modern Mondy Bazar/project/Cropedetail.js
@@ -4,7 +4,40 @@ const { xml } = owl.tags;
 export class Cropedetail extends Component
 {
 	static template = xml`
-		<div class="box">  
-			<h2>Crope Registration Detail</h2>
+		<div  class="card" style="width: 18rem;">   
+			<h2>Crop Registration Detail</h2>
+			<form>
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Farmer Id</label>
+			    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+			    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Crope Name</label>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			  </div>
+			   <div class="form-group">
+			    <label for="exampleInputPassword1">Detail</label>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			  </div>
+			   <div class="form-group">
+			    <label for="exampleInputPassword1">Place</label>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			  </div>
+			   <div class="form-group">
+			    <label for="exampleInputPassword1">QTY in KG</label>
+			    <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			  </div>
+			   <div class="form-group">
+			    <label for="exampleInputPassword1">Category</label>
+			    <input type="select" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			  </div>
+			   <div class="form-group">
+			    <label for="exampleInputPassword1">Keyborad</label>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			  </div>
+  				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
 		</div>`;
 }
+
