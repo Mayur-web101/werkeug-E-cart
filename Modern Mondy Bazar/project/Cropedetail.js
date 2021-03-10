a98eddbcf54a8ba7448ae1a0624275b522a4bd4d
Mayur-web101 <mayur101gvp@gmail.com>
2021-03-10 18:48:34 +0530

    contain change in two file Contain and footer File

--- Modern Mondy Bazar/project/Cropedetail.js
+++ Modern Mondy Bazar/project/Cropedetail.js
@@ -4,7 +4,7 @@ const { xml } = owl.tags;
 export class Cropedetail extends Component
 {
 	static template = xml`
-		<div  class="card" style="width: 18rem;">   
+		<div class="container my-4">   
 			<h2>Crop Registration Detail</h2>
 			<form>
 			  <div class="form-group">
@@ -14,27 +14,27 @@ export class Cropedetail extends Component
 			  </div>
 			  <div class="form-group">
 			    <label for="exampleInputPassword1">Crope Name</label>
-			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Crop Name"/>
 			  </div>
 			   <div class="form-group">
 			    <label for="exampleInputPassword1">Detail</label>
-			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Detail"/>
 			  </div>
 			   <div class="form-group">
 			    <label for="exampleInputPassword1">Place</label>
-			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Place"/>
 			  </div>
 			   <div class="form-group">
 			    <label for="exampleInputPassword1">QTY in KG</label>
-			    <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			    <input type="number" class="form-control" id="exampleInputPassword1" placeholder="QTY"/>
 			  </div>
 			   <div class="form-group">
 			    <label for="exampleInputPassword1">Category</label>
-			    <input type="select" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			    <input type="select" class="form-control" id="exampleInputPassword1" placeholder="Category"/>
 			  </div>
 			   <div class="form-group">
 			    <label for="exampleInputPassword1">Keyborad</label>
-			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+			    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Keyboard"/>
 			  </div>
   				<button type="submit" class="btn btn-primary">Submit</button>
 			</form>
