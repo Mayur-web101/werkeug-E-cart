47348efe088b8977d9b7f81c526ebc37da14d288
Mayur-web101 <mayur101gvp@gmail.com>
2021-04-28 19:22:25 +0530

    contain chnage in  files templet.xml,models.py,routes

--- Modern Mondy Bazar/Demo/mandy_bazar/controller/routes.py
+++ Modern Mondy Bazar/Demo/mandy_bazar/controller/routes.py
@@ -31,3 +31,61 @@ class main(http.Controller):
             'mobile_no': kwargs.get("mobno"),
             })
         return http.local_redirect('/login')
+@http.route('/client_signup_submit', type="http", method="POST", csrf=False)
+    def client_signup_submit(self, **kwargs):
+        # print(kwargs)
+        # import pdb; pdb.set_trace()
+        request.env['client'].create({
+            'role': "client",
+            'email': kwargs.get("email"),
+            'name': kwargs.get("fname"),
+            'password': kwargs.get("password"),
+            'address': kwargs.get("address"),
+            'mobile_no': kwargs.get("mobno"),
+            })
+        return http.local_redirect('/login')
+
+    @http.route('/farmer_crop_registartion', type="http")
+    def farmer_crop_registartion(self, **kwargs):
+        return request.render('mandy_bazar.farmer_crop_registartion')
+
+    @http.route('/crop_registartion', type="http", method="POST", csrf=False)
+    def crop_registartion(self, **kwargs):
+        # print(kwargs)
+        # import pdb; pdb.set_trace()
+        request.env['crop_registration'].create({
+            'farmer_id': kwargs.get("farmer_id"),
+            'crop_name': kwargs.get("cropname"),
+            'detail': kwargs.get("detail"),
+            'place': kwargs.get("place"),
+            'expected_qty': kwargs.get("qty"),
+            'category': kwargs.get("category"),
+            'state': kwargs.get("state"),
+            })
+
+        return http.local_redirect('/farmer_crop_registartion')
+
+    @http.route('/stage_activity', type="http")
+    def stage_activity(self, **kwargs):
+        return request.render('mandy_bazar.stage_activity')
+    
+    @http.route('/stage_activity_add', type="http", method="POST", csrf=False)
+    def stage_activity_add(self, **kwargs):
+        # print(kwargs)
+        # import pdb; pdb.set_trace()
+        request.env['crop_registration'].create({
+            'farmer_id': kwargs.get("farmer_id"),
+            'crop_id': kwargs.get("crop_id"),
+            'cropname': kwargs.get("cropname"),
+            'stage': kwargs.get("stage"),
+            'start_date': kwargs.get("start_date"),
+            'end_date': kwargs.get("end_date"),
+            'price': kwargs.get("price"),
+            'sequence': kwargs.get("sequence"),
+            'video': kwargs.get("video"),
+            'images': kwargs.get("images"),
+            'description': kwargs.get("description"),
+            'finish_note': kwargs.get("Finish_note"),
+            })
+
+        return http.local_redirect('/stage_activity_add')
--- Modern Mondy Bazar/Demo/mandy_bazar/models/models.py
+++ Modern Mondy Bazar/Demo/mandy_bazar/models/models.py
@@ -10,3 +10,59 @@ class farmer(models.Model):
     name = fields.Char(string="Name")
     mobile_no = fields.Char(string="mobile_no")
     password = fields.Char(string="password")
+
+class client(models.Model):
+    _name = 'client'
+    _description = "clients detail"
+
+    # order_id = fields.Many2one('orders', string="orders")
+    role = fields.Selection([('client', 'Client')], string="Role")
+    email = fields.Char(string="Email id")
+    name = fields.Char(string="Name")
+    mobile_no = fields.Char(string="mobile_no")
+    password = fields.Char(string="password")
+    address = fields.Char(string="Address") 
+
+class crop_registration(models.Model):
+    _name = 'crop_registration'
+    _description = "crop detail"
+
+    # order_id = fields.Many2one('orders', string="orders")
+    farmer_id = fields.Many2one('farmer', string="farmer_id")
+    crop_name = fields.Char(string="crop_name")
+    detail = fields.Char(string="detail")
+    place = fields.Char(string="place")
+    expected_qty = fields.Char(string="expected_qty")
+    category = fields.Char(string="Category")
+    state = fields.Char(string="state") 
+
+class crop_registration(models.Model):
+    _name = 'crop_registration'
+    _description = "crop detail"
+
+    # order_id = fields.Many2one('orders', string="orders")
+    farmer_id = fields.Many2one('farmer', string="farmer_id")
+    crop_name = fields.Char(string="crop_name")
+    detail = fields.Char(string="detail")
+    place = fields.Char(string="place")
+    expected_qty = fields.Char(string="expected_qty")
+    category = fields.Char(string="Category")
+    state = fields.Char(string="state") 
+
+class stage_activity_detail(models.Model):
+    _name = 'stage_activity_detail'
+    _description = "stage detail"
+
+    # order_id = fields.Many2one('orders', string="orders")
+    farmer_id = fields.Many2one('farmer', string="farmer_id")
+    crop_id = fields.Many2one('crop_registration', string="crop_id")
+    cropname = fields.Char(string="cropname")
+    stage = fields.Char(string="stage")
+    start_date = fields.Char(string="start_date")
+    end_date = fields.Char(string="end_date")
+    price = fields.Integer(string="price")
+    sequence = fields.Integer(string="sequence") 
+    video = fields.Char(string="video")
+    images = fields.Char(string="images")
+    description = fields.Char(string="description") 
+    finish_note = fields.Char(string="finish_note")
