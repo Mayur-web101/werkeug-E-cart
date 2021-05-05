bf466328dd5bef882b6a81a60b60f56714c58b52
Mayur-web101 <mayur101gvp@gmail.com>
2021-05-05 18:56:25 +0530

    contain added in two files employee.py and web_templet.xml

--- Modern Mondy Bazar/Trainee/trainee_demo/models/employee.py
+++ Modern Mondy Bazar/Trainee/trainee_demo/models/employee.py
@@ -12,3 +12,21 @@ class Student(models.Model):
     age=fields.Char(string='Age')
     designation=fields.Char(string='designation')
     salary=fields.Char(string='salary')
+    image = fields.Binary(string="Image", attachment=True)
+    company_id=fields.Many2one('emlpoyee.company', string="company")
+    hobbies_id=fields.Many2many('employee.hobbies')
+
+class company(models.Model):
+
+    _name = 'emlpoyee.company'
+    _description = 'employee company_list'
+
+    company_name=fields.Char('Company Name')
+    Address=fields.Char('Address')
+    emp_record = fields.One2many('employee.list','company_id',string="employee_record")
+
+class Hobbies(models.Model):
+	_name='employee.hobbies'
+
+	emp_Hobbies=fields.Char(string="Hobbies")
+
