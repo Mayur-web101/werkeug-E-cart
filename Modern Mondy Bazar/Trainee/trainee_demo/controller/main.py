bbc0972f56ae53a755734c2ef6dde39218b3e3ca
Mayur-web101 <mayur101gvp@gmail.com>
2021-05-12 18:47:36 +0530

    contain changes in files main.py employee_report.xml

--- Modern Mondy Bazar/Trainee/trainee_demo/controller/main.py
+++ Modern Mondy Bazar/Trainee/trainee_demo/controller/main.py
@@ -4,7 +4,22 @@ from odoo.http import request
 
 class main(http.Controller):
 
-	@http.route('/mypath', type="http", website=True)
+	@http.route('/home', type="http", website=True)
 	def mypath(self, **kwargs):
 		employees = request.env['employee'].search([])
-		return request.render('trainee_demo.employee_form_view', {'employees':employees})
+		return request.render('trainee_demo.emp_template', {'employees':employees})
+
+	@http.route('/create_employee', type="http", website=True)
+	def create_employee(self, **kwargs):
+		company = request.env['company'].search([])
+		return request.render('trainee_demo.create_employee', {'company': company})
+
+	@http.route('/submit_form', type="http", website=True)
+	def submit_form(self, **kwargs):
+		request.env['employee'].create(kwargs)
+		return http.local_redirect('/home')
+
+	@http.route('/delete/<model("employee"):emp>', type="http", website=True)
+	def delete(self, emp, **kwargs):
+		emp.unlink()
+		return http.local_redirect('/home')
